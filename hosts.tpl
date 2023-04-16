55b802599e90f1403a966914872882fbc76120cc
viktor.abudihin <viktor.abudihin@realize.dev>
2023-04-16 11:35:15 +0300

    first commit

---
+++ hosts.tpl
@@ -0,0 +1,27 @@
+[iscsi_hosts]
+%{ for hostname in iscsi_hosts ~}
+${hostname}
+%{ endfor ~}
+
+[nginx_hosts]
+%{ for hostname in nginx_hosts ~}
+${hostname}
+%{ endfor ~}
+
+[backend_hosts]
+%{ for hostname in backend_hosts ~}
+${hostname}
+%{ endfor ~}
+
+[db_hosts]
+%{ for hostname in db_hosts ~}
+${hostname}
+%{ endfor ~}
+
+[haproxy_host]
+%{ for hostname in haproxy ~}
+${hostname}
+%{ endfor ~}
+
+[ansible_host]
+localhost
---
+++ outputs.tf
@@ -0,0 +1,27 @@
+output "external_ip_address_lb" {
+  value = yandex_lb_network_load_balancer.lb01.listener.*
+}
+
+output "external_ip_address_haproxy" {
+  value = yandex_compute_instance.haproxy.*.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_ansible" {
+  value = yandex_compute_instance.ansible.*.network_interface.0.nat_ip_address
+}
+
+output "internal_ip_address_iscsi" {
+  value = yandex_compute_instance.iscsi.*.network_interface.0.ip_address
+}
+
+output "internal_ip_address_nginx" {
+  value = yandex_compute_instance.nginx.*.network_interface.0.ip_address
+}
+
+output "internal_ip_address_backend" {
+  value = yandex_compute_instance.backend.*.network_interface.0.ip_address
+}
+
+output "internal_ip_address_db" {
+  value = yandex_compute_instance.db.*.network_interface.0.ip_address
+}
